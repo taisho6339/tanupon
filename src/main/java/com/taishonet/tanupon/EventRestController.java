cb08aa40d16e28e84aa0884a48d53120604dd267
Hiroki Sakamoto <taisho6339>
2016-06-12 12:28:56 +0900

    受け口を修正

--- src/main/java/com/taishonet/tanupon/EventRestController.java
+++ src/main/java/com/taishonet/tanupon/EventRestController.java
@@ -1,8 +1,13 @@
 package com.taishonet.tanupon;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by taisho6339 on 16/06/12.
@@ -11,11 +16,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/event")
 public class EventRestController {
 
-    @RequestMapping(value = "/receive", method = RequestMethod.POST)
-    public Message receiveEvent(String json) {
-        System.out.println();
-        System.out.println(json);
-        System.out.println();
-        return new Message();
+    private static final Logger LOG = LoggerFactory.getLogger(EventRestController.class);
+
+    @RequestMapping(value = "/receive", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public SendMessage receiveEvent(@RequestBody String json) {
+        LOG.debug("Receive Request");
+        LOG.debug(json);
+        return new SendMessage();
     }
 }
--- src/main/java/com/taishonet/tanupon/SendMessage.java
+++ src/main/java/com/taishonet/tanupon/SendMessage.java
@@ -3,9 +3,9 @@ package com.taishonet.tanupon;
 /**
  * Created by taisho6339 on 16/06/12.
  */
-public class Message {
-    public String to;
+public class SendMessage {
+    public String to = "";
     public String toChannel = "1383378250";
     public String eventType = "138311608800106203";
-    public String content;
+    public String content = "";
 }
